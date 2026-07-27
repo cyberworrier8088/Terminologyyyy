cd2c38cce0edd0a95cd63d0c2d410e7f42a727c5
cyberworrier8088 <cyberworrier39@gmail.com>
2026-07-27 13:03:18 +0530

    init the window

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use winit::{
+    application::ApplicationHandler,
+    event::WindowEvent,
+    event_loop::{ActiveEventLoop, EventLoop},
+    window::{Window, WindowAttributes},
+};
+
+struct App {
+    window: Option<Window>,
+}
+
+impl ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        let window = event_loop.create_window(WindowAttributes::default().with_title("Terminologyyyya")).unwrap();
+
+        self.window = Some(window);
+    }
+
+    fn window_event(&mut self, event_loop: &ActiveEventLoop, _window_id: winit::window::WindowId, event: WindowEvent) {
+        match event {
+            WindowEvent::CloseRequested => event_loop.exit(),
+             _ => (),
+        }
+    }
+}
+
+fn main() {
+    let event_loop = EventLoop::new().unwrap();
+
+    let mut app = App {
+        window: None,
+    };
+
+    event_loop.run_app(&mut app).unwrap();
+}
+
